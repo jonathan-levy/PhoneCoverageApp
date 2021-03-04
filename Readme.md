e575d510db1a3ac06e6a2fdee70a17758aa2d970
Jonathan LEVY <j.levy@vocalcom.com>
2021-03-04 03:47:00 +0100

    project commit

---
+++ Readme.md
@@ -0,0 +1,31 @@
+# PhoneCoverageApp
+
+PhoneCoverageApp is a ExpressJs app that check phone coverage for a given address.
+
+## Prerequesite
+
+Have [node](nodejs.org) install
+
+## Installation
+
+Go to the repository root
+
+```bash
+cd PhoneCoverageApp
+```
+
+Use the package manager npm to install the app.
+
+```bash
+npm install
+```
+Run the server
+
+```bash
+node index.js
+```
+
+## Usage
+
+```
+Navigate to localhost:3000
---
+++ index.js
@@ -0,0 +1,91 @@
+//express library for node js
+const express = require('express');
+const app = express();
+const port = 3000;
+
+//allows to parse incoming request bodies
+const bodyParser = require('body-parser');
+
+//allows to easily make request
+const request = require("request");
+
+//phone Tower database
+const dataTower = require('./dataTower.json');
+
+//Result object
+let operateur = { 'Orange': { 'On': 0, '2G': 0, '3G': 0, '4G': 0 }, 'SFR': { 'On': 0, '2G': 0, '3G': 0, '4G': 0 }, 'Bouygue': { 'On': 0, '2G': 0, '3G': 0, '4G': 0 }, 'Free': { 'On': 0, '2G': 0, '3G': 0, '4G': 0 }, 'ArrOperateur': ['Orange', 'SFR', 'Bouygue', 'Free'], 'Display': 0, 'Error': 0 };
+
+// func to check distance between two points
+function dist(x1, x2, y1, y2) { return Math.sqrt((x1 - x2) ** 2 + (y1 - y2) ** 2) };
+
+// set the view engine to ejs
+app.set('views', './pages');
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/', function (req, res) {
+    res.render('index', { data: operateur });
+});
+
+app.post('/', (req, res) => {
+    //reset result obj between each call
+    operateur = { 'Orange': { 'On': 0, '2G': 0, '3G': 0, '4G': 0 }, 'SFR': { 'On': 0, '2G': 0, '3G': 0, '4G': 0 }, 'Bouygue': { 'On': 0, '2G': 0, '3G': 0, '4G': 0 }, 'Free': { 'On': 0, '2G': 0, '3G': 0, '4G': 0 }, 'ArrOperateur': ['Orange', 'SFR', 'Bouygue', 'Free'], 'Display': 0, 'Error': 0 };
+
+    //encode input address
+    const uri = req.body;
+    const encoded = encodeURI(uri.item);
+    const urlToQuery = 'https://api-adresse.data.gouv.fr/search/?q=' + encoded + '&limit=1';
+
+    //Call the api that gives coordonates for an address
+    request(urlToQuery, function (error, response, body) {
+        if (!error && response.statusCode == 200) {
+
+            //log response body
+            console.log('body: ' + body);
+
+            var bodyParsed = JSON.parse(body);
+
+            for (let i = 0; i < dataTower.length; i++) {
+                //Bad adress err check
+                if (bodyParsed.features[0] === undefined) {
+                    operateur['Error'] = 1;
+                    res.render('index', { data: operateur });
+                    return null;
+                }
+
+                //Get coordonates of tower and address
+                var x1 = bodyParsed.features[0].properties.x;
+                var x2 = dataTower[i].X;
+                var y1 = bodyParsed.features[0].properties.y;
+                var y2 = dataTower[i].Y;
+
+                //Compare coordonates between tower and address
+                var distance = dist(x1, x2, y1, y2);
+
+                //if tower is in distance get operateur and phone coverage
+                if (distance < 5000) {
+                    var contextOperateur = dataTower[i].Operateur;
+                    operateur[contextOperateur]['On'] = 1;
+                    if (dataTower[i]['2G']) {
+                        operateur[contextOperateur]['2G'] = 1;
+                    }
+                    if (dataTower[i]['3G']) {
+                        operateur[contextOperateur]['3G'] = 1;
+                    }
+                    if (dataTower[i]['4G']) {
+                        operateur[contextOperateur]['4G'] = 1;
+                    }
+                }
+            }
+            //flag to display the result
+            operateur.Display = 1;
+        }
+        res.render('index', { data: operateur });
+    });
+});
+
+//listen to port
+app.listen(port, () => {
+    console.log(`Phone coverage app listening at http://localhost:${port}`);
+})
